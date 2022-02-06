34e24e31e87e028e9bb7037094ba1761cb2e5ce0
gyanasluitel <gyanasluitel@gmail.com>
2022-02-06 20:55:17 +0545

    adding routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,23 @@
 import './App.css';
-import Container from 'react-bootstrap/Container';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { BrowserRouter as Router } from 'react-router-dom';
+// import Container from 'react-bootstrap/Container';
+
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import AppNavbar from './components/layout/AppNavbar';
+import Home from './components/Home';
+import SignIn from './components/user-access/SignIn';
+import SignUp from './components/user-access/SignUp';
 
 function App() {
   return (
     <Router>
       <AppNavbar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='signin' element={<SignIn />} />
+        <Route path='signup' element={<SignUp />} />
+      </Routes>
     </Router>
   );
 }
---
+++ client/src/components/contacts/ContactList.jsx
@@ -0,0 +1,3 @@
+function ContactList() {}
+
+export default ContactList;
